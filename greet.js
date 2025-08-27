6985aeec0fca6c0637f3bc59a8a47f12fedca72b
shahid <shahidibnulatheef786@gmail.com>
2025-08-27 13:56:38 +0530

    score page adedd

--- greet.js
+++ greet.js
@@ -1,3 +1,11 @@
 const greeting = document.getElementById('greeting');
 const name = sessionStorage.getItem('userName') || 'ضيف';
 greeting.textContent = `Assalamu alikum ${name}`;
+const nextBtn = document.getElementById('nextBtn');
+
+if (nextBtn) {
+  nextBtn.addEventListener('click', () => {
+    const manId = document.getElementById('manId').value; // optional
+    window.location.href = `top-scorers.html?manId=${manId}`;
+  });
+}
